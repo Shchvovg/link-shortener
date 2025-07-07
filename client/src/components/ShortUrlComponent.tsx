9bcbf4fbbff77f5b7517abbf95711b4afc6ed3f2
Shchvovg <seryozhatur@gmail.com>
2025-07-07 20:23:11 +0300

    0.2.1

--- client/src/components/ShortUrlComponent.tsx
+++ client/src/components/ShortUrlComponent.tsx
@@ -3,12 +3,12 @@ function ShortUrlComponent ({shortUrl = ''}) {
     const BASE_URL: string = import.meta.env.VITE_BASE_URL;
 
     async function handleButtonClick() {
-        await navigator.clipboard.writeText(shortUrl);
+        await navigator.clipboard.writeText(BASE_URL+'/'+shortUrl);
     }
 
     return(
         <div className="short-url">
-            <label>Your URL: {BASE_URL+shortUrl}</label>
+            <label>Your URL: {BASE_URL+'/'+shortUrl}</label>
             <button onClick={() => handleButtonClick()}>Copy</button>
         </div>
     );
