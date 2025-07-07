7275bd856df6d7f401efcb2b0803459ee507d05d
Shchvovg <seryozhatur@gmail.com>
2025-07-07 20:07:21 +0300

    0.1.6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const API_URL: string = import.meta.env.VITE_API_URL;
   return (
    <div className="page-layout-grid">
-    <UrlForm port={5050} url={API_URL}/>
+    <UrlForm url={API_URL}/>
    </div>
   );
 }
